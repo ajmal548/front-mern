ec19f9f863a2720235eba4cc3663048033bfe1cc
ajmal548 <ajmalknlr2001@gmail.com>
2024-05-30 18:11:38 +0530

    page update

--- src/app/(vendor)/Product/page.tsx
+++ src/app/(vendor)/Product/page.tsx
@@ -1,24 +1,28 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
+import Edit from "../edit/page";
+
+type ProductData = {
+  _id: string;
+  name: string;
+  brand: string;
+  price: number;
+  quantity: number;
+  category_id: string;
+};
 
 const Product = () => {
-  type ProductData = {
-    name: string;
-    brand: string;
-    price: number;
-    quantity: number;
-    category_id: string;
-  };
-
   const [products, setProducts] = useState<ProductData[]>([]);
+  const [update, setUpdate] = useState(false);
+  const [select, setSelect] = useState<ProductData | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const res = await fetch("http://localhost:4000/product/guest");
         const data = await res.json();
-        setProducts(data); 
+        setProducts(data);
       } catch (error) {
         console.error("fetching error data", error);
       }
@@ -26,32 +30,55 @@ const Product = () => {
     fetchData();
   }, []);
 
+  const editProduct = (product: ProductData) => {
+    setSelect(product);
+    setUpdate(true);
+  };
+
   return (
     <div>
       <div className='flex justify-between items-center'>
         <h1 className='text-3xl font-semibold text-fuchsia-700'>Products</h1>
-        <Link href={'addProduct'} className='p-3 bg-sky-600 text-white font-medium hover:bg-sky-400 hover:text-black rounded-md'>
+        <Link href={'/addProduct'} className='p-3 bg-sky-600 text-white font-medium hover:bg-sky-400 hover:text-black rounded-md'>
           Add Product
         </Link>
       </div>
       <div className='py-5 px-2'>
         <ul className='grid grid-cols-2 gap-4'>
           {products.map((product, index) => (
-            <li key={index} className='bg-fuchsia-200 p-3 rounded-md over-flow'>
-              <p className='text-xl font-medium'>Name : {product.name}</p>
-              <p>Brand : {product.brand}</p>
-              <p>Price : {product.price}</p>
-              <p>Category : {product.category_id}</p>
-              <p>Quantity : {product.quantity}</p>
+            <li key={index} className='bg-fuchsia-200 p-3 rounded-md overflow-hidden'>
+              <p className='text-xl font-medium'>Name: {product.name}</p>
+              <p>Brand: {product.brand}</p>
+              <p>Price: {product.price}</p>
+              <p>Category: {product.category_id}</p>
+              <p>Quantity: {product.quantity}</p>
               <div className='flex justify-end'>
-                <Link href={'/edit'}>
-                  <svg className="h-5 w-5 text-red-500" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <path d="M9 7 h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />  <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />  <line x1="16" y1="5" x2="19" y2="8" /></svg>
+                <Link href={"#"}
+                  onClick={() => editProduct(product)}
+                  className='flex items-center'
+                >
+                  <svg className="h-5 w-5 text-red-500" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" />
+                    <path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
+                    <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
+                    <line x1="16" y1="5" x2="19" y2="8" />
+                  </svg>
                 </Link>
               </div>
             </li>
           ))}
         </ul>
-
+        <div>
+          {update && select && (
+            <Edit 
+              product={select}
+              onClose={() => {
+                setUpdate(false);
+                setSelect(null);
+              }}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
--- src/app/(vendor)/edit/page.tsx
+++ src/app/(vendor)/edit/page.tsx
@@ -1,36 +1,299 @@
-"use client"
-import React, { useState } from 'react'
+"use client";
+import React, { useEffect, useState } from 'react';
 
-const Edit = () => {
-  const [name, setName] = useState('name');
-  const [successMessage, setSuccessMessage] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+type ProductData = {
+  _id: string;
+  name: string;
+};
+
+type EditProductProps = {
+  product: ProductData | null;
+  onClose: () => void;
+};
+
+const Edit: React.FC<EditProductProps> = ({ product, onClose }) => {
+  const [id, setId] = useState('');
+  const [name, setName] = useState('');
+  const [category_id, setCategory_id] = useState('');
+  const [brand, setBrand] = useState('');
+  const [color, setColor] = useState('');
+  const [warranty_info, setWarranty_info] = useState('');
+  const [price, setPrice] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [unit, setUnit] = useState('');
+  const [pack_of_quantity, setPack_of_quantity] = useState('');
+  const [boosting, setBoosting] = useState('');
+  const [stock, setStock] = useState('stock');
+  const [status, setStatus] = useState('active');
+  const [description, setDescription] = useState('');
+  const [vendor_id, setVendor_id] = useState('');
+  const [review_id, setReview_id] = useState('');
+  const [picture, setPicture] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    if (product) {
+      setId(product._id);
+      setName(product.name);
+      setCategory_id
+      setBrand
+      setColor
+      setWarranty_info
+      useState
+      setPack_of_quantity
+      setBoosting
+      setDescription
+      setVendor_id
+      setReview_id
+      setPicture
+      
+    }
+  }, [product]);
+
+  const updateProduct = async (event: React.FormEvent) => {
+    event.preventDefault();
+    if (!product) {
+      setErrorMessage("Product data is not available");
+      return;
+    }
+    const url = `http://localhost:4000/product/${id}`;
+    try {
+      const res = await fetch(url, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, category_id, brand, color, warranty_info, price, quantity, unit,
+          pack_of_quantity, boosting, stock, status, description, picture }),
+      });
+      if (res.ok) {
+        setSuccessMessage("Product updated successfully");
+        setErrorMessage("");
+      } else {
+        const errorData = await res.json();
+        setErrorMessage(errorData.message || "Failed to update product");
+        setSuccessMessage("");
+      }
+    } catch (error) {
+      setErrorMessage("Error updating product");
+      setSuccessMessage("");
+    }
+  };
+
   return (
     <>
-      <form className="space-y-4"
-      //onSubmit={updateProduct}
-      >
-        <div className="col-span-1">
-          <label className="block mb-2 text-sm font-medium text-gray-700">Product Name</label>
-          <input
-            type="text"
-            name="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded-md"
-            placeholder="Product Name"
-          />
-        </div>
+      <div className="justify-center items-center  overflow-x-hidden overflow-y-auto fixed inset-0 z-50  bg-red-200  p-5">
+      <form className="space-y-4" onSubmit={updateProduct}>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="col-span-1">
+              <label className="block mb-2 text-sm font-medium text-gray-700">Product Name</label>
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-md"
+                placeholder="Product Name"
+              />
+            </div>
+            <div className="col-span-1">
+              <label className="block mb-2 text-sm font-medium text-gray-700">Category</label>
+              <input
+                type="text"
+                name="category"
+                value={category_id}
+                onChange={(e) => setCategory_id(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-md"
+                placeholder="Category"
+              />
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="col-span-1">
+              <label className="block mb-2 text-sm font-medium text-gray-700">Brand</label>
+              <input
+                type="text"
+                name="brand"
+                value={brand}
+                onChange={(e) => setBrand(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-md"
+                placeholder="Brand"
+              />
+            </div>
+            <div className="col-span-1">
+              <label className="block mb-2 text-sm font-medium text-gray-700">Color</label>
+              <input
+                type="text"
+                name="color"
+                value={color}
+                onChange={(e) => setColor(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-md"
+                placeholder="Color"
+              />
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="col-span-1">
+              <label className="block mb-2 text-sm font-medium text-gray-700">Warranty Info</label>
+              <input
+                type="text"
+                name="warranty"
+                value={warranty_info}
+                onChange={(e) => setWarranty_info(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-md"
+                placeholder="Warranty"
+              />
+            </div>
+            <div className="col-span-1">
+              <label className="block mb-2 text-sm font-medium text-gray-700">Price</label>
+              <input
+                type="text"
+                name="price"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-md"
+                placeholder="Price"
+              />
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <div className="col-span-1">
+              <label className="block mb-2 text-sm font-medium text-gray-700">Stock Quantity</label>
+              <input
+                type="number"
+                name="stock"
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-md"
+                placeholder="Stock Quantity"
+              />
+            </div>
+            <div className="col-span-1">
+              <label className="block mb-2 text-sm font-medium text-gray-700">Unit</label>
+              <input
+                type="number"
+                name="unit"
+                value={unit}
+                onChange={(e) => setUnit(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-md"
+                placeholder="Unit"
+              />
+            </div>
+            <div className="col-span-1">
+              <label className="block mb-2 text-sm font-medium text-gray-700">Pack of Quantity</label>
+              <input
+                type="number"
+                name="pfquantity"
+                value={pack_of_quantity}
+                onChange={(e) => setPack_of_quantity(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-md"
+                placeholder="Pack of Quantity"
+              />
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <div className="col-span-1">
+              <label className="block mb-2 text-sm font-medium text-gray-700">Boosting</label>
+              <input
+                type="text"
+                name="boosting"
+                value={boosting}
+                onChange={(e) => setBoosting(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-md"
+                placeholder="Boosting"
+              />
+            </div>
+            <div className="col-span-1">
+              <label className="block text-gray-700">Stock Status</label>
+              <select
+                name="stock"
+                value={stock}
+                onChange={(e) => setStock(e.target.value)}
+                className="w-full px-3 py-2 border rounded-md"
+              >
+                <option value="stock">In Stock</option>
+                <option value="outofstock">Out of Stock</option>
+              </select>
+            </div>
+            <div className="col-span-1">
+              <label className="block text-gray-700">Status</label>
+              <select
+                name="status"
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+                className="w-full px-3 py-2 border rounded-md"
+              >
+                <option value="active">Active</option>
+                <option value="inactive">Inactive</option>
+              </select>
+            </div>
+          </div>
+          <div>
+            <label className="block mb-2 text-sm font-medium text-gray-700">Description</label>
+            <textarea
+              name="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded-md"
+              placeholder="Description"
+            />
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="col-span-1">
+              <label className="block mb-2 text-sm font-medium text-gray-700">Vendor ID</label>
+              <input
+                type="text"
+                name="name"
+                value={vendor_id}
+                onChange={(e) => setVendor_id(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-md"
+                placeholder="Product Name"
+              />
+            </div>
+            <div className="col-span-1">
+              <label className="block mb-2 text-sm font-medium text-gray-700">Review</label>
+              <input
+                type="text"
+                name="category"
+                value={review_id}
+                onChange={(e) => setReview_id(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-md"
+                placeholder="Category"
+              />
+            </div>
+            </div>
+          <div>
+            <label className="block mb-2 text-sm font-medium text-gray-700">Product Images</label>
+            <input
+              type="file"
+              name="images"
+              value={picture}
+              multiple
+              onChange={(e) => setPicture(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded-md"
+            />
+          </div>
 
         <div className="flex justify-end space-x-4">
           {successMessage && <p className="text-green-600 mb-4">{successMessage}</p>}
           {errorMessage && <p className="text-red-600 mb-4">{errorMessage}</p>}
-          <button type="button" className="px-4 py-2 bg-gray-300 rounded-md">Cancel</button>
-          <button type="submit" className="px-4 py-2 bg-blue-600 text-white rounded-md">Save</button>
+          <button
+            type="button"
+            onClick={onClose}
+            className="px-4 py-2 bg-gray-300 rounded-md"
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="px-4 py-2 bg-blue-600 text-white rounded-md"
+          >
+            Save
+          </button>
         </div>
       </form>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Edit
+export default Edit;
